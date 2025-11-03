2e27773fc30fd737a8a873d66e22607dcc480c06
whqlskqTy2 <kimhhss06@gmail.com>
2025-11-03 15:33:32 +0900

    [feat] 업적 시스템

--- Assets/Script/CoinItem.cs
+++ Assets/Script/CoinItem.cs
@@ -27,6 +27,9 @@ public class CoinItem : InteractablObject
         {
             QuestManager.Instance.AddCollectProgress(questTag);
         }
+
+        AchievementManager.instance?.UpdateProgress(AchievementType.CollectCoins, coinValue);
+
         transform.Rotate(Vector3.up * 180f);
         Destroy(gameObject, 0.5f);
     }
